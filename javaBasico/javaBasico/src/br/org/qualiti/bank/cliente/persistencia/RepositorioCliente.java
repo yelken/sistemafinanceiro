a10aab1d9813107d3823bd7cef07596871d9d062
yelkezin@gmail.com <yelkezin@gmail.com@cc44e4a0-fe53-11de-89cc-8daf83ecd611>
2010-01-14 04:45:15 +0000

    Criado a interface de cliente, falta criar o repositorio armazenando num array e tratando algumas regras na classe RepositorioContaArray

---
+++ javaBasico/javaBasico/src/br/org/qualiti/bank/cliente/persistencia/RepositorioCliente.java
@@ -0,0 +1,20 @@
+package br.org.qualiti.bank.cliente.persistencia;
+
+import br.org.qualiti.bank.cliente.excecoes.ClienteExistenteException;
+import br.org.qualiti.bank.cliente.excecoes.ClienteInexistenteException;
+import br.org.qualiti.bank.cliente.model.Cliente;
+
+public interface RepositorioCliente {
+
+	public void inserir(Cliente cliente) throws ClienteExistenteException;
+	
+	public void atualizar(Cliente cliente) throws ClienteInexistenteException;
+	
+	public void remover(Cliente cliente) throws ClienteInexistenteException;
+	
+	public boolean existe(Cliente cliente);
+	
+	public Cliente pesquisar(Cliente cliente);
+	
+	public Cliente[] listar();
+}

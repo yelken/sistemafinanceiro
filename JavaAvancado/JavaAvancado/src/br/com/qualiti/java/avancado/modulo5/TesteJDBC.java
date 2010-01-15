d2f70c0db8956a499c10529a3e3d066b418e5965
yelkezin <yelkezin@cc44e4a0-fe53-11de-89cc-8daf83ecd611>
2010-01-15 00:51:08 +0000

    Criado a classe de conexão com o banco de dados e outra classe que gerencia a conexão aberta pela aplicação

---
+++ JavaAvancado/JavaAvancado/src/br/com/qualiti/java/avancado/modulo5/TesteJDBC.java
@@ -0,0 +1,25 @@
+package br.com.qualiti.java.avancado.modulo5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteJDBC {
+	public static void main(String[] args) {
+	    try {
+	      // Driver sendo carregado 
+	      Class.forName("org.hsqldb.jdbcDriver");
+	      // Obtendo uma conexão com o banco de dados
+	      Connection con = 		DriverManager.getConnection(
+				"jdbc:hsqldb:hsql://localhost:9090/qib ","sa", "");
+	      /* Neste ponto as consultas já poderiam ser executadas,  
+			 uma vez que a conexão foi estabelecida com sucesso */
+	      // Fechando a conexão obtida
+	      con.close();
+	    } catch (ClassNotFoundException e) {
+			// Não foi possível carregar classe do Driver
+	    } catch (SQLException e) {
+	       // SQL Exception
+	    }
+	  }
+}

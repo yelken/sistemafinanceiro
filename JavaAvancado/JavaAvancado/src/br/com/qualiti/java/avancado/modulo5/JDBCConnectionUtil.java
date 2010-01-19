47ee755b3ca16e0663b8f053b16910ce9f363071
yelkezin@gmail.com <yelkezin@gmail.com@cc44e4a0-fe53-11de-89cc-8daf83ecd611>
2010-01-19 23:18:51 +0000

    Criado uma classe só para conexão

--- JavaAvancado/JavaAvancado/src/br/com/qualiti/java/avancado/modulo5/JDBCConnectionUtil.java
+++ JavaAvancado/JavaAvancado/src/br/com/qualiti/java/avancado/modulo5/JDBCConnectionUtil.java
@@ -11,45 +11,25 @@ import java.util.Properties;
 
 
 public class JDBCConnectionUtil {
-	private static String driver;
-	private static String url;
-	private static String user;
-	private static String pass;
+	
+	// criar uma classe só pra conexão yelken 19/01/2010
+	private static String driver = "org.hsqldb.jdbcDriver";
+	private static String url = "jdbc:hsqldb:hsql://localhost:9090/qib";
+	private static String user = "sa";
+	private static String pass = "";
 	
 	private static Connection connection;
 	
-	static {
-		//Tenta carregar os dados da conexao
-		File file = new File("jdbc.properties");    
-		Properties props = new Properties();
-		FileInputStream fis = null;
-		try {
-			fis = new FileInputStream(file);
-			//lê os dados que estão no arquivo
-			props.load(fis);
-			driver = props.getProperty("driver");
-			url = props.getProperty("url");
-			user = props.getProperty("user");
-			pass = props.getProperty("pass");
-			fis.close();
-		}catch (IOException ex) {
-			System.out.println(ex.getMessage());
-			ex.printStackTrace();
-		} 
-		
-		//Tenta carregar o driver do servidor de banco de dados
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			System.out.println("Erro ao carregar o driver JDBC do HSQLDB. ");
-			System.exit(-1);
-		}
-	}
-	
 	public static Connection getConnection() throws SQLException{
-		if (connection == null || connection.isClosed()){
+		if (connection != null){
 			//Tenta estabelecer um,a conexão com o servidor de Banco de dados
-			connection = DriverManager.getConnection(url, user, pass);
+			try {
+				Class.forName("org.hsqldb.jdbcDriver");
+			} catch (ClassNotFoundException e) {
+					
+			}
+		      // Obtendo uma conexão com o banco de dados
+		      connection = DriverManager.getConnection(url,user,pass);
 		}
 		return connection;
 	}
